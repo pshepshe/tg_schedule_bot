c6444e269dd03370ba9d49c2b4081ece5ba5e51e
Denis <denis_loshkarev>
2020-11-19 22:44:27 +0400

    create mcs_helper_bot in telegram

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,21 @@
-#test
+import telebot
+
+bot = telebot.TeleBot('1475926428:AAHcGfNcx0VpOvPkiQt6_1EuRjajyMo86UY')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    print(message)
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет!')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай!')
+    else:
+        bot.send_message(message.chat.id, '-')
+
+bot.polling()
