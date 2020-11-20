4d5e80aabee0fdf69aec66cd4c6208fdad7761f0
Denis <denis_loshkarev>
2020-11-20 22:20:28 +0400

    update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -3,19 +3,34 @@ import telebot
 bot = telebot.TeleBot('1475926428:AAHcGfNcx0VpOvPkiQt6_1EuRjajyMo86UY')
 
 
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Привет', 'Пока', 'Стикер')
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    print(message)
+    bot.send_message(message.chat.id, 'start', reply_markup=keyboard1)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     print(message)
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет!')
+        bot.send_message(message.chat.id, 'Привет')
     elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай!')
+        bot.send_message(message.chat.id, 'Прощай')
+    elif message.text.lower() == 'стикер':
+        bot.send_sticker(message.chat.id, 'CAACAgQAAxkBAAO_X7gH9in8dNjxPQxr-yGrckY8tF4AAhkBAAImEKQPn2FCCxlUWG8eBA')
     else:
         bot.send_message(message.chat.id, '-')
+
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
+    print(message.sticker.file_id)
+    bot.send_sticker(message.chat.id, message.sticker.file_id)
+
 
 bot.polling()
