33445db808dcbe2e7076ad6b8fdbf8f5cde957ad
Denis <denis_loshkarev>
2020-11-21 22:09:28 +0400

    update bot buttons

--- bot/bot.py
+++ bot/bot.py
@@ -3,14 +3,31 @@ import telebot
 bot = telebot.TeleBot('1475926428:AAHcGfNcx0VpOvPkiQt6_1EuRjajyMo86UY')
 
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Привет', 'Пока', 'Стикер')
+day_of_week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота']
+
+keyboard_start = telebot.types.ReplyKeyboardMarkup()
+keyboard_start.row('/start')
+keyboard_start.row('Расписание')
+keyboard_start.row('Привет', 'Пока', 'Стикер')
+keyboard_start.row('Выбор группы')
+
+keyboard_days = telebot.types.ReplyKeyboardMarkup()
+keyboard_days.row('Понедельник', 'Вторник')
+keyboard_days.row('Среда', 'Четверг')
+keyboard_days.row('Пятница', 'Суббота')
+keyboard_days.row('Полное расписание')
+keyboard_days.row('Назад')
+
+keyboard_change_course = telebot.types.ReplyKeyboardMarkup()
+keyboard_change_course.row('1', '2')
+keyboard_change_course.row('3', '4')
+keyboard_change_course.row('Назад')
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     print(message)
-    bot.send_message(message.chat.id, 'start', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'start', reply_markup=keyboard_start)
 
 
 @bot.message_handler(content_types=['text'])
@@ -22,8 +39,19 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Прощай')
     elif message.text.lower() == 'стикер':
         bot.send_sticker(message.chat.id, 'CAACAgQAAxkBAAO_X7gH9in8dNjxPQxr-yGrckY8tF4AAhkBAAImEKQPn2FCCxlUWG8eBA')
+    elif message.text.lower() == 'расписание':
+        bot.send_message(message.chat.id, 'Выбери день недели', reply_markup=keyboard_days)
+    elif message.text.lower() in day_of_week:
+        day_number = str(day_of_week.index(message.text.lower()))
+        bot.send_message(message.chat.id, str(day_number), reply_markup=keyboard_days)
+    elif message.text.lower() == 'полное расписание':
+        bot.send_message(message.chat.id, 'all', reply_markup=keyboard_days)
+    elif message.text.lower() == 'выбор группы':
+        bot.send_message(message.chat.id, 'Выбери номер курса', reply_markup=keyboard_change_course)
+    elif message.text.lower() == 'назад':
+        bot.send_message(message.chat.id, '-', reply_markup=keyboard_start)
     else:
-        bot.send_message(message.chat.id, '-')
+        bot.send_message(message.chat.id, 'Такая команда не существует')
 
 
 @bot.message_handler(content_types=['sticker'])
