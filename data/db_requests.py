1dc5d98edc69e60ed5fde8a1e1dde0a2947303ef
pshepshe <71153364+pshepshe@users.noreply.github.com>
2020-11-30 00:02:38 +0300

    добавил функцию, которая выводит расписание

--- data/db_requests.py
+++ data/db_requests.py
@@ -3,7 +3,7 @@ import datetime
 
 
 def time_in_right_form():
-    """ Вовращает время в корректном формате
+    """ Вовращает текущее время в корректном формате
 
     :return: строка со временем
     """
@@ -29,19 +29,18 @@ def choose_users_by_time(db_cursor, time):
     :param time: время в корректном формате
     :return: массив с id студентов
     """
-    group = cursor.execute('SELECT group_id FROM schedule WHERE ' + time + ' != ""').fetchall()
+    group = db_cursor.execute('SELECT group_id FROM schedule WHERE ' + time + ' != ""').fetchall()
     students_id = []
     for group_number in range(len(group)):
         one_group_id = db_cursor.execute('SELECT id FROM users WHERE group_number = ' +
                           '"' + group[group_number][0] + '"').fetchall()
         for id_number in range(len(one_group_id)):
             students_id.append(one_group_id[id_number][0])
-
     return students_id
 
 
 def add_user_to_table(db_cursor, user_id, group):
-    """Добавляет id пользователя в базу данных
+    """Добавляет id пользователя и его группу в базу данных
 
     :param db_cursor: курсор базы данных
     :param user_id: id пользователя
@@ -56,3 +55,41 @@ def add_user_to_table(db_cursor, user_id, group):
                           'WHERE id = ' + str(user_id))
         print('note was updated')
 
+
+def get_schedule(db_cursor, user_id):
+    """ Выводит расписание в виде двумерного массива вида schedule[a], где a - принимает значения от 0 до 6 и каждая
+    цифра, начиная с 0 означает день недели.
+
+    :param db_cursor: курсор базы данных
+    :param user_id: id пользователя, который принимается в виде строки
+    :return: лист с расписанием
+    """
+    # массив с информацией о стобцах
+    group = db_cursor.execute('SELECT group_number FROM users WHERE id =' + user_id).fetchall()
+    group = '"' + group[0][0] + '"'
+    list_of_time = db_cursor.execute('PRAGMA table_info(schedule)').fetchall()
+    counter_of_lectures = 1
+    schedule = []
+    for day_of_week in range(6):
+        schedule.append('')
+        while list_of_time[counter_of_lectures][1][0] == str(day_of_week):
+            current_time = list_of_time[counter_of_lectures][1]
+            current_lecture = db_cursor.execute('SELECT ' + '"' + current_time + '"' +
+                                                ' FROM schedule WHERE group_id = ' + group).fetchall()[0][0]
+            # для исключения из листа промежутков отсутствия лекций
+            if current_lecture != '':
+                schedule[day_of_week] = schedule[day_of_week] + current_time[1:6:1] + ' ' + current_lecture + '\n'
+            counter_of_lectures += 1
+            if counter_of_lectures >= len(list_of_time):
+                break
+    return schedule
+
+
+connection = sqlite3.connect('users_data.db')
+cursor = connection.cursor()
+
+add_user_to_table(cursor, 123, '20.Б01-мкн')
+connection.commit()
+# пример работы
+b = get_schedule(cursor, str(123))
+print(b[0])
