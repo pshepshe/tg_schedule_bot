67c154b379d33c33de633dcbb0a8e7de78038fe8
pshepshe <71153364+pshepshe@users.noreply.github.com>
2020-12-21 03:57:35 +0300

    дописал алгоритм для того, чтобы календарь обновлялся

--- data/update_calendar.py
+++ data/update_calendar.py
@@ -1,7 +1,7 @@
 import httplib2
 import apiclient
 from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime
+from datetime import datetime, timedelta
 
 groups_calendar = {'20.Б01-мкн': 'dcdq0k481p3ank5cg4nkjauti8@group.calendar.google.com',
                    '20.Б02-мкн': '',
@@ -25,7 +25,7 @@ service = apiclient.discovery.build('calendar', 'v3', http=httpAuth)
 
 event = {
     "summary": 'test_event',
-    "description": 'просто тест',
+    "description": 'none',
     'start': {
         'dateTime': '2020-12-20T02:00:00',
         'timeZone': 'Europe/Moscow',
@@ -37,6 +37,7 @@ event = {
 }
 #service.events().insert(calendarId=calendar_ident, body=event).execute()
 current_date = datetime.today().date()
+current_day = datetime.today().weekday()
 print(current_date)
 
 with open('schedule_data2.csv', 'r') as schedule:
@@ -48,18 +49,23 @@ with open('schedule_data2.csv', 'r') as schedule:
         schedule_table.append(current_line)
         current_line = schedule.readline().split('|')
 events = service.events().list(calendarId='dcdq0k481p3ank5cg4nkjauti8@group.calendar.google.com').execute()
-for time_line in schedule_table:
-    for group_number in range(len(time_line) - 2):
-        current_lecture = time_line[group_number + 2]
-        check_event_existence = 0
-        current_lecture = 'test_event'
-        for current_event in events['items']:
-            if current_lecture == current_event['summary']:
-                check_event_existence = 1
-        if (current_lecture != '') & (current_lecture != '\n') & (check_event_existence == 0):
-            event['summary'] = current_lecture
-            event['start']['dateTime'] = str(current_date) + 'T' + current_line[1][:5]
-            event['end']['dateTime'] = str(current_date) + 'T' + current_line[1][8:13]
-            current_group = groups[group_number + 2]
-            #service.events().insert(calendarId=groups_calendar[current_group], body=event).execute()
+for delta_day in range(7):
+    day = str((int(datetime.today().weekday()) + delta_day) % 7)
+    for time_line in schedule_table:
+        for group_number in range(len(time_line) - 2):
+            current_lecture = time_line[group_number + 2]
+            check_event_existence = 0
+            date = current_date + timedelta(days=delta_day)
+            for current_event in events['items']:
+                kek1 = current_event['start']['dateTime'][:10]
+                kek2 = current_event['summary']
+                if (current_lecture == current_event['summary']) & (str(date) == current_event['start']['dateTime'][:10]):
+                    check_event_existence = 1
+            if (current_lecture != '') & (current_lecture != '\n') & (check_event_existence == 0) & (time_line[0] == day):
+                event['summary'] = current_lecture
+                event['start']['dateTime'] = str(date) + 'T' + time_line[1][:5] + ':00'
+                event['end']['dateTime'] = str(date) + 'T' + time_line[1][8:] + ':00'
+                current_group = groups[group_number + 2]
+                if groups_calendar[current_group] != '':
+                    service.events().insert(calendarId=groups_calendar[current_group], body=event).execute()
 
